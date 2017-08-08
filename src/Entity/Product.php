879753ab1bdbd72974ef0d4bc65686f280f67cc7
emilien letestu <eletestu@gmail.com>
2017-08-09 00:25:48 +0200

    last commit for fully functional overview

---
+++ src/Entity/Product.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 08/08/17
+ * Time: 23:05
+ */
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="product")
+ */
+class Product
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    public $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    public $name;
+
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
+    public $price;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    public $description;
+}
